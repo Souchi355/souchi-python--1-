dc8056383ff1e402c781aa0da69416b12ab070dd
rayen <rayenbenyoussef355@gmail.com>
2022-12-18 10:19:02 +0100

    2

--- class.py
+++ class.py
@@ -7,4 +7,13 @@ class student:
         self.phoneN=phoneN
         self.adress=adress
         
+    def add_email(self,email):
+        self.email=email
+    def add_phoneN(self,pn):
+        self.phoneN=pn
+    def add_adress(self,ad):
+        self.adress=ad
         
+s1=student("001","rayen",17)
+s1.add_email("rayenbenyoussef355@gmail.com")
+print(f"student id: {s1.id} ,student name: {s1.name} ,student age: {s1.age} ,student email: {s1.email}\nstudent phone number: {s1.phoneN} ,student adress: {s1.adress}")
