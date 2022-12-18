34ac77259c3c8a047e9670193cf9a636cafde32c
rayen <rayenbenyoussef355@gmail.com>
2022-12-18 10:05:18 +0100

    yy

---
+++ class.py
@@ -0,0 +1,10 @@
+class student:
+    def __init__(self,id,name,age,email=None,phoneN=None,adress=None):
+        self.id=id
+        self.name=name
+        self.age=age
+        self.email=email
+        self.phoneN=phoneN
+        self.adress=adress
+        
+        
