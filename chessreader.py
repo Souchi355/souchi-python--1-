07491daf19a4ec1a7de2393c015ea91ce29d6131
Souchi355 <90983402+Souchi355@users.noreply.github.com>
2022-12-25 11:46:20 +0100

    Update chessreader.py

--- chessreader.py
+++ chessreader.py
@@ -16,6 +16,15 @@ class chess:
             listm[i]=moves[moves.find(str(i)+"."):moves.find(str(i+1)+".")]
         print(listm)
         
-moves='''[itSe "Chess.com"][Result "*"]1. e4 d6 2. c3 Nf6 3. d4 Bg4 4. f3 Bh5 5. Qd2 g6 6. f4 Nxe4 7. Qe2 d5 8. Qe3 Bh6 9. Bb5+ c6 10. Ba4 Bg5 11. fxg5 f6 12. Bc2 f5 13. Nd2 f4 14. Qxf4 Rf8 15. Qe3 h616. gxh6 b5 17. a4 a6 18. axb5 cxb5 19. Bxe4 dxe4 20. Qxe4 g5 21. Qxa8 Qc822. Rxa6 Qxa6 23. Qd5 Nc6 24. Qxg5 Bf7 25. Qg7 Rg8 26. Qh7 Qa1 27. Kd1 Rxg228. Qe4 Rg6 29. h7 Kd7 30. Nb3 Rh6 31. Nxa1 e6 32. Bxh6 Bh5+ 33. Nf3 e5 34. h8=Qexd4 35. cxd4 Kd6 36. Qh4 Bxf3+ 37. Ke1 Bxh1 38. Q4d8+ Ke6 39. Qb6 Kd5 40. Qxb5+Kd6 41. Qd8+ Ke6 42. Qc7 Nxd4 43. Nb3 Bd5 44. Nxd4+ Kf6 45. Qxd5 Kg6 46. Nf5 Kf647. Qg7# * '''
+moves='''[Site "Chess.com"]
+[Result "*"]
+1. e4 d6 2. c3 Nf6 3. d4 Bg4 4. f3 Bh5 5. Qd2 g6 6. f4 Nxe4 7. Qe2 d5 8. Qe3 Bh6
+9. Bb5+ c6 10. Ba4 Bg5 11. fxg5 f6 12. Bc2 f5 13. Nd2 f4 14. Qxf4 Rf8 15. Qe3 h6
+16. gxh6 b5 17. a4 a6 18. axb5 cxb5 19. Bxe4 dxe4 20. Qxe4 g5 21. Qxa8 Qc8
+22. Rxa6 Qxa6 23. Qd5 Nc6 24. Qxg5 Bf7 25. Qg7 Rg8 26. Qh7 Qa1 27. Kd1 Rxg2
+28. Qe4 Rg6 29. h7 Kd7 30. Nb3 Rh6 31. Nxa1 e6 32. Bxh6 Bh5+ 33. Nf3 e5 34. h8=Q
+exd4 35. cxd4 Kd6 36. Qh4 Bxf3+ 37. Ke1 Bxh1 38. Q4d8+ Ke6 39. Qb6 Kd5 40. Qxb5+
+Kd6 41. Qd8+ Ke6 42. Qc7 Nxd4 43. Nb3 Bd5 44. Nxd4+ Kf6 45. Qxd5 Kg6 46. Nf5 Kf6
+47. Qg7# *'''
 chs=chess(moves)
 chs.splt(chs.moves)
