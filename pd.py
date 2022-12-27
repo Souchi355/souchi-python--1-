7f3afc78ac86a09e159d4d168db901274f6965ad
Souchi355 <90983402+Souchi355@users.noreply.github.com>
2022-12-27 12:27:11 +0100

    Add files via upload

--- pd.py
+++ pd.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-file="board.xcls"
+file="board.xlsx"
+idxn=[1,2,3,4,5,6,7,8]
+idxl=["A","B","C","D","E","F","G","H"]
+df=pd.read_excel(file,index_col="N/L")
+for n in idxn:
+    for l in idxl:
+        if df.loc[n,l] !="_._":
+            df.loc[n,l]=df.loc[n,l][0:2]
+print(df)
